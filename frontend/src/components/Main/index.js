8a19e0e48e0cead6d84d3fc684fc39163f9e1afe
Fardosa Garad <fardosamgarad@gmail.com>
2022-02-16 19:15:01 -0500

    nutrition

--- frontend/src/components/Main/index.js
+++ frontend/src/components/Main/index.js
@@ -4,10 +4,12 @@ import './main.css';
 const Main = () => {
   return (
     <>
+          <div >
           
+          </div>
             <h3>Welcome to Divine Energy</h3>
-            <h4>Our aim is to promote healthy living and the importance of womans health.</h4>
-            {/* <img className="banner" src="/images/homepage-banner.png" /> */}
+            <h4>Our aim is to promote healthy living and to shed light on the importance of womans health.</h4>
+            <img className="banner" src="/images/divine-energy-banner.png" />
           
     </>
   )
--- frontend/src/components/Nutrition/index.js
+++ frontend/src/components/Nutrition/index.js
@@ -1,6 +1,53 @@
-import React from 'react';
-import './nutrition.css';
+import React from "react";
+import "./nutrition.css";
 
 export default function Nutrition() {
-  return <h2 className="nutrition">My Nutrition</h2>;
+  return (
+    <div>
+      <h2 className="nutrition">MEAL PLANS</h2>
+
+      <p className="intro">
+        Meal plans are just as important as fitness. We need to ensure we are
+        eating right, in order to feel right. Eat good, feel good.
+      </p>
+
+      <div className="flex-container">
+        <div>
+          <p className="breakfast-heading"> Breakfast </p>
+          <p className="breakfast-meals">
+            {"\n"} Here are some delicious protein filled breakfast options
+            <ul>
+              <li>Avocado toast with egg</li>
+              <li>Flax seed oatmeal</li>
+              <li>Yogurt parfait with assorted berries</li>
+            </ul>
+          </p>
+        </div>
+
+        <div>
+          Lunch
+          <p>
+            {"\n"} Here are some delicious protein filled lunch options
+            <ul>
+              <li>Coffee</li>
+              <li>Tea</li>
+              <li>Milk</li>
+            </ul>
+          </p>
+        </div>
+
+        <div>
+          Dinner
+          <p>
+            {"\n"} Here are some delicious protein filled dinner options
+            <ul>
+              <li>Coffee</li>
+              <li>Tea</li>
+              <li>Milk</li>
+            </ul>{" "}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
