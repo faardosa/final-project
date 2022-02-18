edf61db64d2b889a1ecb2e82f1d5d952ca3e21e5
faardosa <93020474+faardosa@users.noreply.github.com>
2022-02-17 19:15:46 -0500

    Fardosa nutrition (#5)
    * nutrition
    
    * added links to the recipes
    
    * meal plans

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,77 @@
-import React from 'react';
-import './App.css';
-import NutritionPage from './routes/NutritionPage';
-import FitnessPage from './routes/FitnessPage';
-import MainPage from './routes/MainPage';
-import MentalHealthPage from './routes/MentalHealthPage';
-
-import {
-    Routes,
-    Route
-  } from "react-router-dom";
-  import Nav from "./components/Navbar/"
-
+import React from "react";
+import "./App.css";
+import NutritionPage from "./routes/NutritionPage";
+import FitnessPage from "./routes/FitnessPage";
+import MainPage from "./routes/MainPage";
+import MentalHealthPage from "./routes/MentalHealthPage";
+import { Routes, Route } from "react-router-dom";
+import Nav from "./components/Navbar/";
+import Recipe from "./components/Nutrition/Recipes";
+import { useState } from "react";
 
 function App() {
+  const [state] = useState({
+    meals: { 
+      
+      breakfast: { 
+      
+        id: "breakfast", 
+        header: "Breakfast",
+        items: [{ name: "Avocado" }, { name: "Oatmeal" }] 
+        
+        },
+        
+        lunch: { 
+      
+
+          id:"lunch",
+          header:  "Lunch",
+          items: [{ name: "Chicken" }, { name: "Salmon" }] 
+        
+        },
+        dinner: { 
+      
+          id:"dinner",
+          header:"Dinner",
+          items: [{ name: "Avocado" }, { name: "Oatmeal" }, { name: "Oatmeal" }] 
+        
+        },
+      },
+
+    recipes: {
+
+        Avocado: {
+          description: "Ingredients: 1 whole ripe avocado, whole wheat slice of bread, 2 eggs, cooked to your way of preference."
+        },
+
+        Oatmeal: {
+          description: "oatmeal"
+        },
+        Chicken: {
+          description: "chicken"
+        },
+        Salmon: {
+          description: "salmon"
+        },
+        
+
+
+
+    }
+  });
+
   return (
     <div className="wrapper">
-      <Nav/>
+      <Nav />
       <Routes>
-      <Route path="/" element={<MainPage />} />
-      <Route path="fitness" element={<FitnessPage />} />
-      <Route path="nutrition" element={<NutritionPage />} />
-      <Route path="mentalhealth" element={<MentalHealthPage />} />
-
-    </Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="fitness" element={<FitnessPage />} />
+        <Route path="nutrition" element={<NutritionPage meals={state.meals} />} />
+        <Route path="mentalhealth" element={<MentalHealthPage />} />
+        <Route path="recipe/:name" element={<Recipe recipes={state.recipes} />} />
+      </Routes>
     </div>
   );
 }
 
-
-
 export default App;
-
--- frontend/src/components/Nutrition/index.js
+++ frontend/src/components/Nutrition/index.js
@@ -1,6 +1,78 @@
-import React from 'react';
-import './nutrition.css';
+import React from "react";
+import "./nutrition.css";
+import { Link } from "react-router-dom";
 
-export default function Nutrition() {
-  return <h2 className="nutrition">My Nutrition</h2>;
+export default function Nutrition({meals}) {
+  const mealsList = Object.values(meals).map((meal)=>{
+      return ( <div>
+        <p className="breakfast-heading"> {meal.header} </p>
+        <p className="breakfast-meals">
+          {"\n"} Here are some delicious protein filled meals to keep you full and satisified throughout the day.
+          <ul>
+            {meal.items.map((item) => {
+                  return ( <li> <Link to ={`/recipe/${item.name}`}> {item.name} </Link> </li> )
+
+              })
+            }
+           
+          </ul>
+        </p>
+      </div>) 
+
+  })
+
+  
+
+  return (
+    <div>
+      <h2 className="nutrition">MEAL PLANS</h2>
+
+      <p className="intro">
+        Meal plans are just as important as fitness. We need to ensure we are
+        eating right, in order to feel right. Eat good, feel good.
+      </p>
+
+      <div className="flex-container">
+        {mealsList}
+
+
+        {/* <div>
+          <p className="breakfast-heading"> Breakfast </p>
+          <p className="breakfast-meals">
+            {"\n"} Here are some delicious protein filled breakfast options
+            <ul>
+              <li> <Link to ="recipe"> Avocado toast with egg </Link>
+              </li>
+              <li>Flax seed oatmeal</li>
+              <li>Yogurt parfait with assorted berries</li>
+            </ul>
+          </p>
+        </div>
+
+        <div>
+          Lunch
+          <p>
+            {"\n"} Here are some delicious protein filled lunch options
+            <ul>
+              <li>Coffee</li>
+              <li>Tea</li>
+              <li>Milk</li>
+            </ul>
+          </p>
+        </div>
+
+        <div>
+          Dinner
+          <p>
+            {"\n"} Here are some delicious protein filled dinner options
+            <ul>
+              <li>Coffee</li>
+              <li>Tea</li>
+              <li>Milk</li>
+            </ul>{" "}
+          </p>
+        </div> */}
+      </div>
+    </div>
+  );
 }
