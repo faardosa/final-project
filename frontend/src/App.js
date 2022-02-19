09d1f07b54b20bab7f530444dfeb5ce13cff338d
sumaya <sumayaomarr@gmail.com>
2022-02-18 20:51:24 -0500

    format navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,15 @@ import ResourcesPage from "./routes/ResourcesPage";
 import { Routes, Route } from "react-router-dom";
 import Nav from "./components/Navbar/";
 import Recipe from "./components/Nutrition/Recipes";
+import Bodyfitness from './components/Fitness/index.js';
+import Arms from './components/Arms/index.js';
+import Legs from './components/Legs/index.js';
+import Abs from './components/Abs/index.js';
+import Back from './components/Back/index.js';
+import Glutes from './components/Glutes/index.js';
+
+
+
 import { useState } from "react";
 
 function App() {
@@ -71,6 +80,14 @@ function App() {
         <Route path="mentalhealth" element={<MentalHealthPage />} />
         <Route path="recipe/:name" element={<Recipe recipes={state.recipes} />} />
         <Route path="resources" element={<ResourcesPage />} />
+        <Route path="Bodyfitness" element={<Bodyfitness/>} />
+      <Route path="Arms" element={<Arms/>} />
+      <Route path="Legs" element={<Legs/>} />
+      <Route path="Abs" element={<Abs/>} />
+      <Route path="Back" element={<Back/>} />
+      <Route path="Glutes" element={<Glutes/>} />
+
+
       </Routes>
     </div>
   );
--- frontend/src/components/Main/index.js
+++ frontend/src/components/Main/index.js
@@ -1,19 +1,28 @@
-import React from "react"
+import React from 'react';
+// import '../App.css';
+import { Button } from '../Button/Button.js';
 import './main.css';
 
-const Main = () => {
+function HeroSection() {
   return (
-    <>
-          <div >
-          
-          </div>
-            <h3>Welcome to Divine Energy</h3>
-            <h4>Our aim is to promote healthy living and to shed light on the importance of womans health.</h4>
-            <img alt="banner" className="banner" src="/images/divine-energy-banner.png" />
-          
-    </>
-  )
+    <div className='hero-container'>
+      <video src='/videos/video-1.mp4' autoPlay loop muted />
+      <h1>THE BODY ACHIEVES</h1>
+      <p>WHAT THE MIND BELIEVES</p>
+     
+      <div className='hero-btns'>
+        <Button
+          className='btns'
+          buttonStyle='btn--outline'
+          buttonSize='btn--large'
+        >
+          GET STARTED
+        </Button>
 
+      
+      </div>
+    </div>
+  );
 }
 
-export default Main
+export default HeroSection;
