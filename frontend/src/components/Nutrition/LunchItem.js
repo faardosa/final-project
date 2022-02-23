04c7fd92e15d61e3a18bcb6c042d02b0fc294944
faardosa <93020474+faardosa@users.noreply.github.com>
2022-02-23 11:58:54 -0500

    final nutrition styling (#17)

--- frontend/src/components/Nutrition/LunchItem.js
+++ frontend/src/components/Nutrition/LunchItem.js
@@ -4,31 +4,27 @@ const LunchItem = (props)=>{
 
 
 return (
-<div className="mainlunch-wrapper"> 
-  <h1>
-   Below are a few lunch meal options.
-  </h1>
 
- 
-<div className="lunch-wrapper"> 
-<div className="lunch-left">
-<p style={{textAlign:"center"}}> {title}</p>
-<p className="lunch-description">  {description} </p>
+  <div className="mainlunch-wrapper"> 
+<h1>
+ Below are a few lunch meal options.
+</h1>
 
-<div className='lunch-recipe-tag'>
-    <a className="lunch-recipe" target="_blank" href={url}>
-    
-    Click here for recipe
-      
-      </a>  
-    
-  </div> 
 
-</div>
 
-<img className="lunch" src={image} alt="lunch" />
-  </div>    
+<div class="lunch-wrapper">
+  <div class="card">
+    <div class="card-img-body">
+    <img className="breakfast" src={image} alt="Card image cap"/>
+    </div>
+    <div class="card-body">
+      <h4 class="card-title">{title}</h4>
+      <p style={{textAlign:"center"}} class="card-text">{description}</p>
+      <a href={url} class="btn btn-outline-primary">Recipe</a>
+  </div>
 
+  </div>
+  </div>
   </div>
 )
 
